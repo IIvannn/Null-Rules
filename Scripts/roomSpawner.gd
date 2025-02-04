d233b9cef71303d1d2ae8499d9e213d48c09da77
IIvannn <56793619+IIvannn@users.noreply.github.com>
2025-02-04 14:43:46 +0100

    git

---
+++ Scripts/roomSpawner.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+@onready var pos = $Marker3D
+
+@onready var room1 = preload("res://scenes/rooms/test/R1.tscn")
+@onready var room2 = preload("res://scenes/rooms/test/R2.tscn")
+@onready var room3 = preload("res://scenes/rooms/test/R3.tscn")
+
+@export var r_array : Array[PackedScene]
+@export var offset : Array[Vector3]
+
+var roomRandomizer = RandomNumberGenerator.new()
+var instantiated = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	var new_pos : Vector3 = pos.position
+	
+	for r in 9:
+		var new_id = roomRandomizer.randi_range(0,2)
+		var new_room = r_array[new_id].instantiate()
+		add_child(new_room)
+		new_room.position = new_pos
+		var new_offset = offset[randi_range(0,1)]
+		new_pos = new_room.position + new_offset
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
